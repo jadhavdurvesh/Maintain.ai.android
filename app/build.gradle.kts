013f793da87cb0d30277a0c3dec76bc5b4419a49
Durvesh <89296288+jadhavdurvesh@users.noreply.github.com>
2026-09-09 19:39:33 +0530

    Fix settings repositories conflict

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,6 @@ android {
 
 kotlin { jvmToolchain(17) }
 
-repositories { google(); mavenCentral() }
-
 dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2026.08.00")
     implementation(composeBom)
