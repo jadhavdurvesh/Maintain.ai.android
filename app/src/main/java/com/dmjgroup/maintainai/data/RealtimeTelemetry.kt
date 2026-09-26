d5eee2a87c60ba0fa5c5a0a39758c081cc3ec079
Durvesh <89296288+jadhavdurvesh@users.noreply.github.com>
2026-09-26 16:14:07 +0530

    Fix realtime BuildConfig import

--- app/src/main/java/com/dmjgroup/maintainai/data/RealtimeTelemetry.kt
+++ app/src/main/java/com/dmjgroup/maintainai/data/RealtimeTelemetry.kt
@@ -1,6 +1,7 @@
 package com.dmjgroup.maintainai.data
 
 import android.util.Log
+import com.dmjgroup.maintainai.BuildConfig
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
